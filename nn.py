89ebb9cdd2809d8a5b02fe6e2dc7d4679a15ece1
Jan Dobrowolski <jan.dobrowolski@mendeley.com>
2017-02-19 19:05:44 +0000

    Creating training, test and validation data sets

--- nn.py
+++ nn.py
@@ -1,8 +1,15 @@
 import numpy as np
+import random
 
 labels = {"Iris-setosa": [1, 0, 0],
           "Iris-versicolor": [0, 1, 0],
           "Iris-virginica": [0, 0, 1]}
+
+data = None
+data_labels = None
+training_data = None
+test_data = None
+validation_data = None
 
 
 def normalize_and_scale_data(data):
@@ -14,12 +21,24 @@ def normalize_and_scale_data(data):
         data[:, column] /= std_deviations[column]
 
 
+def generate_data_sets():
+    global training_data, test_data, validation_data
+    data_count = data.shape[0]
+    indices = [i for i in range(data_count)]
+    random.shuffle(indices)
+    training_data = indices[0:int(0.6 * data_count)]
+    test_data = indices[int(0.6 * data_count):int(0.6 * data_count) + int(0.2 * data_count)]
+    validation_data = indices[-int(0.2 * data_count):]
+
+
 def run():
-    data, data_labels = read_data("iris.data")
+    read_data("iris.data")
     normalize_and_scale_data(data)
+    generate_data_sets()
 
 
 def read_data(data_file):
+    global data, data_labels
     f = open(data_file, "r")
     data = []
     data_labels = dict()
@@ -27,7 +46,7 @@ def read_data(data_file):
         split_line = line.replace("\n", "").split(",")
         data.append(split_line[:-1])
         data_labels[index] = labels[split_line[-1:][0]]
-    return np.array(data, dtype=np.float64), data_labels
+    data = np.array(data, dtype=np.float64)
 
 
 if __name__ == "__main__":
